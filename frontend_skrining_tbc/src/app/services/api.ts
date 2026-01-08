f0a6f4e8da5bd0a954c584d4dffd10778e2475b8
Heigel.muhammad <heigeloptcrs@gmail.com>
2026-01-08 23:26:15 +0700

    ganti api url untuk production

--- frontend_skrining_tbc/src/app/services/api.ts
+++ frontend_skrining_tbc/src/app/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 // Ganti URL ini jika backend Anda berjalan di port atau host yang berbeda
-const API_URL = "http://127.0.0.1:5000"
+const API_URL = "tugasakhirskriningtbc-production.up.railway.app"
 
 const api = axios.create({
   baseURL: API_URL,

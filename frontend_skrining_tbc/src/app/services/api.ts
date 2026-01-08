b2e7913ed742a43dc06647fe4f4bb6183e3da27f
Heigel.muhammad <heigeloptcrs@gmail.com>
2026-01-08 23:32:18 +0700

    ganti api ur

--- frontend_skrining_tbc/src/app/services/api.ts
+++ frontend_skrining_tbc/src/app/services/api.ts
@@ -1,13 +1,12 @@
-import axios from "axios"
+import axios from "axios";
 
-// Ganti URL ini jika backend Anda berjalan di port atau host yang berbeda
-const API_URL = "tugasakhirskriningtbc-production.up.railway.app"
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://tugasakhirskriningtbc-production.up.railway.app";
 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
-})
+});
 
-export default api
+export default api;

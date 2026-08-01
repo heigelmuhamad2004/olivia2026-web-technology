d8a202ffddfa21779b0003d0c7f0753355a09591
Heigel.muhammad <heigeloptcrs@gmail.com>
2026-08-01 18:06:48 +0700

    Fix URL axios fallback menggunakan proxy /api_flask

--- frontend_skrining_tbc/src/lib/axios.ts
+++ frontend_skrining_tbc/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 // Impor helper untuk mendapatkan token yang aktif
 import { getActiveToken } from "@/app/services/auth.services";
 // Ambil base URL dari environment variable, dengan fallback ke localhost
-const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:5000";
+const baseURL = process.env.NEXT_PUBLIC_API_URL || "/api_flask";
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
